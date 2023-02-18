c29c5529932b66e57b0decb1e47a395ce0f12a9f
Joachim Vanneste <2460800V@student.gla.ac.uk>
2023-02-18 15:26:24 +0000

    step method added

--- q_learning_agent.py
+++ q_learning_agent.py
@@ -1,6 +1,8 @@
 import numpy as np
 import sys
 
+global maze
+
 maze = np.asarray([['+', '-', '+', '-', '+', '-', '+'],
         ['|', ' ', ' ', ' ', ' ', ' ', '|'],
         ['+', ' ', '+', '-', '+', ' ', '+'],
@@ -10,11 +12,8 @@ maze = np.asarray([['+', '-', '+', '-', '+', '-', '+'],
         ['+', '-', '+', '-', '+', '-', '+']])
 
 maze = np.where((maze=='+')|(maze=='-')|(maze=='|'), 1, maze)
-index = np.where(maze == '$')
-
 print(maze)
-print(index)
-print(maze[index])
+
 
 def up(index):
     return (index[0]-1, index[1])
@@ -31,9 +30,29 @@ def right(index):
 
 
 
-
-sys.exit()
 def step(state, action):
+    done = False
+    new_state = maze[action(state)]
+    print(new_state)
+    if new_state!=[' ']:
+        print('wall')
+        # if we hit a wall
+        new_state = state
+        reward = -1
 
+    elif new_state==['F']:
+        print('finished')
+        # if we are finished
+        done = True
+        reward = 10
+
+    else:
+        print('space')
+        # move
+        reward = 0
 
     return new_state, done, reward
+
+
+state = np.where(maze == '$')
+step(state, right)
